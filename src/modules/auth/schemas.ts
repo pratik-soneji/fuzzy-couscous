1f69d421b841529c34e5a9231fee0da7e2712a16
ITUchihha <pratiksoneji95@gmail.com>
2026-01-06 23:50:21 +0530

    09-auth-state

--- src/modules/auth/schemas.ts
+++ src/modules/auth/schemas.ts
@@ -3,12 +3,11 @@ export const registerSchema =  z.object({
     email: z.string().email(),
     password: z.string(),
     username: z.string()
-    .min(3,"Username must be at least 3 characters")
+    .min(3,"username must be of three charcters")
     .max(63,"username must be less than 63 characters")
     .regex(
         /^[a-z0-9][a-z0-9-]*[a-z0-9]$/,
-        "Username can only contain lowercase letters, numbers, and hyphens. It must start and end with a letter or number"
-    )
+        "Username only contain lowercase letters, numbers and hypthens. It must start and end with a letter or a number"
     )
     .refine(
         (val)=>(!val.includes("--")),
--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -14,3 +14,5 @@ export const trpc = createTRPCOptionsProxy({
   queryClient: getQueryClient,
 });
 
+
+export const caller =  appRouter.createCaller(createTRPCContext)
